027b54286baf46817b8e7a554ed608ab33cbca87
Patrick Reagan <patrick.reagan@viget.com>
2009-09-02 20:38:41 -0400

    Add module-level methods to use the correct class for a given URL

--- lib/snip_snap.rb
+++ lib/snip_snap.rb
@@ -1,6 +1,7 @@
 $:.unshift File.dirname(__FILE__)
 
 require 'curb'
+require 'uri'
 
 require 'snip_snap/client'
 
@@ -8,3 +9,25 @@ require 'snip_snap/skitch'
 require 'snip_snap/imgly'
 require 'snip_snap/yfrog'
 require 'snip_snap/twitpic'
+
+module SnipSnap
+  
+  def self.host_map
+    {
+      'skitch.com'  => 'Skitch',
+      'img.ly'      => 'Imgly',
+      'twitpic.com' => 'Twitpic',
+      'yfrog.com'   => 'Yfrog'
+    }
+  end
+
+  def self.factory(url)
+    const_get(class_name_for(url)).new(url)
+  end
+  
+  def self.class_name_for(url)
+    uri = URI.parse(url)
+    host_map[uri.host]
+  end
+  
+end
--- test/unit/snip_snap_test.rb
+++ test/unit/snip_snap_test.rb
@@ -1,13 +1,57 @@
-# require File.dirname(__FILE__) + '/../test_helper'
-# 
-# class SnipSnapTest < Test::Unit::TestCase
-# 
-#   describe "An instance of the SnipSnap class" do
-# 
-#     it "should flunk" do
-#       flunk "Please provide some tests"
-#     end
-#     
-#   end
-# 
-# end
+require File.dirname(__FILE__) + '/../test_helper'
+
+class SnipSnapTest < Test::Unit::TestCase
+
+  describe "The SnipSnap module" do
+
+    should "know the correct class name for a Skitch URL" do
+      url = 'http://skitch.com/reagent/bh4ei/bleeergh'
+      SnipSnap.class_name_for(url).should == 'Skitch'
+    end
+    
+    should "know the correct class name for an Imgly URL" do
+      url = 'http://img.ly/3ey'
+      SnipSnap.class_name_for(url).should == 'Imgly'
+    end
+    
+    should "know the correct class name for a Twitpic URL" do
+      url = 'http://twitpic.com/203o0'
+      SnipSnap.class_name_for(url).should == 'Twitpic'
+    end
+    
+    should "know the correct class name for a Yfrog URL" do
+      url = 'http://yfrog.com/ahb97j'
+      SnipSnap.class_name_for(url).should == 'Yfrog'
+    end
+    
+    should "be able to create an instance of the Skitch class with the supplied URL" do
+      url = 'http://skitch.com/reagent/bh4ei/bleeergh'
+      SnipSnap::Skitch.expects(:new).with(url).returns('skitch')
+      
+      SnipSnap.factory(url).should == 'skitch'
+    end
+    
+    should "be able to create an instance of the Imgly class with the supplied URL" do
+      url = 'http://img.ly/3ey'
+      SnipSnap::Imgly.expects(:new).with(url).returns('imgly')
+      
+      SnipSnap.factory(url).should == 'imgly'
+    end
+    
+    should "be able to create an instance of the Twitpic class with the supplied URL" do
+      url = 'http://twitpic.com/203o0'
+      SnipSnap::Twitpic.expects(:new).with(url).returns('twitpic')
+
+      SnipSnap.factory(url).should == 'twitpic'
+    end
+
+    should "be able to create an instance of the Yfrog class with the supplied URL" do
+      url = 'http://yfrog.com/ahb97j'
+      SnipSnap::Yfrog.expects(:new).with(url).returns('yfrog')
+
+      SnipSnap.factory(url).should == 'yfrog'
+    end
+    
+  end
+
+end
